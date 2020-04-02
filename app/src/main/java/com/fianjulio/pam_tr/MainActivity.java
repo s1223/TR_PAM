fba8a87703a53f34567121f5e0879bca1e702ac7
s1223 <fianjulio@gmail.com>
2020-04-02 20:24:39 +0700

    test

--- app/src/main/java/com/fianjulio/pam_tr/MainActivity.java
+++ app/src/main/java/com/fianjulio/pam_tr/MainActivity.java
@@ -2,7 +2,11 @@ package com.fianjulio.pam_tr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final GridView gridview = (GridView) findViewById(R.id.gridv);
+
+        gridview.setAdapter(new ImagesAdapter(this));
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
     }
 }
