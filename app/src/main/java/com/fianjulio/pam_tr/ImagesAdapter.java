c616de26a8eb7d4dd1a98547d5600c28c343e003
s1223 <fianjulio@gmail.com>
2020-04-03 21:58:02 +0700

    dah beres masalah kemarin

--- app/src/main/java/com/fianjulio/pam_tr/ImagesAdapter.java
+++ app/src/main/java/com/fianjulio/pam_tr/ImagesAdapter.java
@@ -13,13 +13,13 @@ public class ImagesAdapter extends BaseAdapter{
     private Context mContext;
     int imageTotal = 7;
     public static String[] mThumbIds = {
-            "http://192.168.31.66/GambarTRPAM/1.jpg",
-            "http://192.168.31.66/GambarTRPAM/2.jpg",
-            "http://192.168.31.66/GambarTRPAM/3.jpg",
-            "http://192.168.31.66/GambarTRPAM/4.jpg",
-            "http://192.168.31.66/GambarTRPAM/5.jpg",
-            "http://192.168.31.66/GambarTRPAM/6.jpg",
-            "http://192.168.31.66/GambarTRPAM/7.jpg",
+            "https://png.clipart.me/istock/previews/3895/38953970-france-national-flag-square-button-isolated-on-white-background.jpg",
+            "https://images.freeimages.com/images/premium/large-thumbs/4616/46163316-denmark-flag-square-glossy-button.jpg",
+            "https://fastly.4sqi.net/img/general/200x200/35684184_UbuzxuKzsCzDRzDNseU1HPc3VlFyrS4AJC9cZtSkxJg.jpg",
+            "https://images.freeimages.com/images/premium/large-thumbs/4621/46210652-namibia-flag-square-glossy-button.jpg",
+            "https://png.clipart.me/istock/previews/4620/46206954-malaysia-flag-square-glossy-button.jpg",
+            "https://fastly.4sqi.net/img/general/200x200/6534561_UZ6fkjMYQXUFf9ZFdAZyl_vq6dh4XTfHigkYRr-rM_k.jpg",
+            "https://ecs7.tokopedia.net/img/cache/200-square/product-1/2015/8/24/74044/74044_d2370465-56df-41cc-9f1b-7a963f179522.jpg",
     };
 
     public ImagesAdapter(Context c) {
